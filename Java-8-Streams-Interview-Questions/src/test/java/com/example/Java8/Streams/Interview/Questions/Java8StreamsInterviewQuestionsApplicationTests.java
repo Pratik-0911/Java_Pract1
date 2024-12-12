575f8cc6126d87d9cc23a8df998a40bb0306d70f
Nikesh <nikeshchimula417@gmail.com>
2024-12-12 10:50:16 +0530

    Added Java 8 Stream Questions with Solutions

---
+++ Java-8-Streams-Interview-Questions/src/test/java/com/example/Java8/Streams/Interview/Questions/Java8StreamsInterviewQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java8.Streams.Interview.Questions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8StreamsInterviewQuestionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
